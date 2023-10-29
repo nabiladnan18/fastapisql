6fafda92404116ae7f7a37fc5e0d9e5448922f29
Nabil Adnan <nabiladnan99@gmail.com>
2023-10-29 21:13:01 +0100

    💃 pydantic model for responses

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 
 from fastapi import FastAPI, status, HTTPException, Depends
 from sqlalchemy.orm import Session
@@ -11,17 +12,25 @@ models.Base.metadata.create_all(bind=engine)
 
 # FastAPI app
 app = FastAPI()
+
+
 @app.get('/')
 async def root():
     return {'message': f'Hello World! Hello from the {os.environ["DB_HOST"]}!'}
 
-@app.get('/posts')
-def get_posts(db: Session=Depends(get_db)):
+
+@app.get('/posts', response_model=List[schemas.PostResponse])
+# If we add `schemas.PostResponse` as the `repsose_model`, it does not work because
+# we are returning a list of posts, whereas the response model tries to fit that
+# into the model for one single post as is defined in PostResponse 🤦‍♂️
+# This is why need to import List[] from typing library
+def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return posts
 
-@app.post('/posts', status_code=status.HTTP_201_CREATED)
-def create_posts(post: schemas.PostCreate, db: Session=Depends(get_db)):
+
+@app.post('/posts', status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # ALT: new_post = models.Post(title=post.title, content=post.content, published=post.published)
     new_post = models.Post(**post.model_dump())
     db.add(new_post)
@@ -29,9 +38,11 @@ def create_posts(post: schemas.PostCreate, db: Session=Depends(get_db)):
     db.refresh(new_post)
     return new_post
 
-@app.get('/posts/{post_id}')
-def get_post(post_id: int, db: Session=Depends(get_db)):
-    fetched_post = db.query(models.Post).get({"id":post_id}) # get finds via PK
+
+@app.get('/posts/{post_id}', response_model=schemas.PostResponse)
+def get_post(post_id: int, db: Session = Depends(get_db)):
+    fetched_post = db.query(models.Post).get(
+        {"id": post_id})  # get finds via PK
     if not fetched_post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -39,32 +50,36 @@ def get_post(post_id: int, db: Session=Depends(get_db)):
         )
     return fetched_post
 
+
 @app.delete('/posts/{post_id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(post_id: int, db: Session=Depends(get_db)):
-    post_to_be_deleted = db.query(models.Post).filter(models.Post.id == post_id).first()
+def delete_post(post_id: int, db: Session = Depends(get_db)):
+    post_to_be_deleted = db.query(models.Post).filter(
+        models.Post.id == post_id).first()
     if not post_to_be_deleted:
         raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"Post with post id: {post_id} not found."
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with post id: {post_id} not found."
         )
     post_to_be_deleted.delete(synchronize_session=False)
     db.commit()
 
-@app.put('/posts/{post_id}')
-def update_post(post_id: int, post: schemas.PostUpdate, db: Session=Depends(get_db)):
+
+@app.put('/posts/{post_id}', response_model=schemas.PostResponse)
+def update_post(post_id: int, post: schemas.PostUpdate, db: Session = Depends(get_db)):
     post_query = db.query(models.Post).filter(models.Post.id == post_id)
     post_to_be_updated = post_query.first()
     if not post_to_be_updated:
         raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"Post with post id: {post_id} not found."
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with post id: {post_id} not found."
         )
     post_query.update(post.model_dump(), synchronize_session=False)
     db.commit()
     return post_query.first()
 
+
 @app.get('/sqlalchemy')
-def test_posts(db: Session=Depends(get_db)):
+def test_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return posts
 
--- app/schemas.py
+++ app/schemas.py
@@ -1,15 +1,36 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 # Defining a pydantic model
 # This creates a validation for data sent by the client side
+
+
 class PostBase(BaseModel):
     title: str
     content: str | int
-    published: bool=True
+    published: bool = True
     # rating: Optional[int]=None # `Optional` module imported from typing not fastapi
+
 
 class PostCreate(PostBase):
     pass
 
+
 class PostUpdate(PostBase):
     pass
+
+
+class PostResponse(PostBase):
+    # These three are inherited from the baseclass
+    title: str
+    content: str
+    published: bool
+    id: int
+    created_at: datetime
+
+    # We add the `class Config` because we would use the ORM to return the data to show to the user,
+    # which will then be an ORM object and not a valid `dict` type
+    # By adding this we tell the the Pydantic model to read the data even if it is not a valid dict
+    class Config:
+        orm_mode = True
