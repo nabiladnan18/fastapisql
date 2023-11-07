65cd37f6f0db6db7033341422156f8dab5136ed1
Nabil Adnan <nabiladnan99@gmail.com>
2023-11-07 23:06:12 +0100

    ℹ️  using routers for users

---
+++ app/routers/users.py
@@ -0,0 +1,47 @@
+from typing import List
+
+from fastapi import status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+
+from app import models, schemas, utils
+from app.database import get_db
+
+router = APIRouter()
+
+
+@router.post('/users', status_code=status.HTTP_201_CREATED,
+             response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    # create a hash for the pw
+    hashed_user_pw = utils.hashed(user.password)
+    # update the pydantic model
+    user.password = hashed_user_pw
+
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+
+@router.get('/users/{user_id}', response_model=schemas.UserResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    # user = db.query(models.User).\
+    #     filter(models.User.id == user_id).first()
+    user = db.query(models.User).get({"id": user_id})
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with id: {user_id} not found"
+        )
+
+    return user
+
+
+@router.get('/users/', response_model=List[schemas.UserResponse])
+def get_users(db: Session = Depends(get_db)):
+    user = db.query(models.User).all()
+
+    return user
