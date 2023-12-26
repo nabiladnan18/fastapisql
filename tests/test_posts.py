4d00089b560a2613aac9687ba277737cacac6149
Nabil Adnan <nabiladnan99@gmail.com>
2023-12-26 19:04:13 +0100

    🥸 test delete and unauthorised user actions

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,5 +1,8 @@
 import pytest
-from app import schemas
+
+from sqlalchemy.exc import NoResultFound
+
+from app import models, schemas
 
 
 def test_get_all_posts(authorised_client, create_test_posts):
@@ -65,4 +68,55 @@ def test_unauthorised_create_one_post(client, test_user, create_test_posts):
     assert response.status_code == 401
 
 
-# def test_delete_post(authorised_client, test_user):
+def test_delete_post_successful(
+    authorised_client, test_user, create_test_posts, session
+):
+    delete_post_id = create_test_posts[1].id
+    response = authorised_client.delete(f"/posts/{delete_post_id}")
+    assert response.status_code == 204
+    with pytest.raises(NoResultFound):
+        assert session.get_one(models.Post, delete_post_id) is False
+
+
+def test_delete_nonexisting_post(authorised_client):
+    response = authorised_client.delete("/posts/9999999")
+    assert response.status_code == 404
+
+
+def test_delete_not_your_post(authorised_client, create_test_posts):
+    response = authorised_client.delete(f"/posts/{create_test_posts[3].id}")
+    assert response.status_code == 401
+
+
+def test_update_post(authorised_client, test_user, create_test_posts):
+    response = authorised_client.put(
+        f"/posts/{create_test_posts[0].id}",
+        json={
+            "title": "changed title",
+            "content": "changed content",
+            "id": create_test_posts[0].id,
+        },
+    )
+    assert response.status_code == 200
+    updated_post = response.json()
+    assert schemas.PostResponse.model_validate(updated_post)
+    assert updated_post["id"] == create_test_posts[0].id
+    assert updated_post["title"] == create_test_posts[0].title
+    assert updated_post["content"] == create_test_posts[0].content
+
+
+def test_unauthorised_delete(client, create_test_posts):
+    response = client.delete(f"/posts/{create_test_posts[3].id}")
+    assert response.status_code == 401
+
+
+def test_unauthorised_update(client, create_test_posts):
+    response = client.put(
+        f"/posts/{create_test_posts[3].id}",
+        json={
+            "title": "changed title",
+            "content": "changed content",
+            "id": create_test_posts[0].id,
+        },
+    )
+    assert response.status_code == 401
